167a2286f1f1b78fcd2660987e7b885f2abbdbed
jgaitanr <jgaitanr@outlook.com>
2022-06-27 10:38:16 -0500

    footer

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -4,7 +4,7 @@ export const AppFooter = (props) => {
 
     return (
         <div className="layout-footer">
-            <img src="/logos/pjudicial.svg" alt="Logo" height="20" className="mr-2" />
+            <img src="/logos/logo.svg" alt="Logo" height="20" className="mr-2" />
             <span className="font-medium ml-2">Jheyson Gaitan - CSJLL</span>
         </div>
     );
