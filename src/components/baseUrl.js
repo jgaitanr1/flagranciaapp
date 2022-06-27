ac9c184a94eac9a2416bac168766443245fb4294
jgaitanr <jgaitanr@outlook.com>
2022-06-27 10:39:40 -0500

    produccion

--- src/components/baseUrl.js
+++ src/components/baseUrl.js
@@ -1,6 +1,6 @@
 const localhostEndpoint = "https://localhost:44355/api/";
 const productionEndpoint = "https://flagranciaapi.azurewebsites.net/api/";
-const defaultEndpoint = localhostEndpoint;
+const defaultEndpoint = productionEndpoint;
 
 const restrictions = {
     MIN_DATE_TO_MAKE_ORDER: new Date()
